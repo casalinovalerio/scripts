5b9e38f38fd94191a56c7c1f7df6d0edf74d25cc
Valerio Casalino <casalinovalerio.cv@gmail.com>
2019-12-29 10:58:11 +0100

    Improved pdfcompress.sh

--- pdfcompress.sh
+++ pdfcompress.sh
@@ -3,14 +3,45 @@
 error() { printf "$1\\n"; exit 1; }
 
 command -v gs || error "You need gs to run this script"
-([ $# -eq 0 ] || [ $# -ge 3 ] ) &&\
-       	error "You need to input at least 1 filename";
 
 infile="$1"
-outfile="output.pdf" 
-[ $# -eq 2 ] && [ "$1" != "$2" ] && outfile="$2"
+tmpfile="$( mktemp )"
 
-gs -sDEVICE=pdfwrite \
-	-dCompatibilityLevel=1.4 \
-	-dNOPAUSE -dQUIET -dBATCH \
-	-sOutputFile="$outfile" "$infile"
+[ -z "$infile" ] && error "$0 [infile] (opt)[outfile] (opt)[quality]"
+
+if [ ! -z "$2" ]; then 
+	outfile="$2"
+else
+	outfile="-" #stdout
+fi
+
+if [ ! -z "$3" ]; then
+	quality="$3"
+else
+	quality="72"
+fi
+
+gs						\
+	-q -dNOPAUSE -dBATCH -dSAFER		\
+	-sDEVICE=pdfwrite			\
+	-dCompatibilityLevel=1.3		\
+	-dPDFSETTINGS=/screen			\
+	-dEmbedAllFonts=true			\
+	-dSubsetFonts=true			\
+	-dAutoRotatePages=/None			\
+	-dColorImageDownsampleType=/Bicubic	\
+	-dColorImageResolution=$quality		\
+	-dGrayImageDownsampleType=/Bicubic	\
+	-dGrayImageResolution=$quality		\
+	-dMonoImageDownsampleType=/Subsample	\
+	-dMonoImageResolution=$quality		\
+	-sOutputFile="$tmpfile"			\
+	"$infile"
+
+[ ! -f "$infile" -o ! -f "$tmpfile" ] && error "No file written"
+
+insize=$( wc -c "$infile" | cut -f1 -d\  )
+outsize=$( wc -c "$tmpfile" | cut -f1 -d\ )
+[ "$insize" -lt "$outsize" ] && error "Input smaller that output"
+
+mv "$tmpfile" "$outfile"
