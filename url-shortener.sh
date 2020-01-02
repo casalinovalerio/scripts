f0327d3900850919c6bdd3a17d880fdb48dcb402
casalinovalerio <casalinovalerio.cv@gmail.com>
2020-01-02 19:32:55 +0100

    Now url-shortener uses an external service

--- url-shortener.sh
+++ url-shortener.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-[ $# -eq 1 ] || exit 1
+###############################################################################
+# Script name		: url-shortener.sh
+# Author		: Even a monkey could do it
+###############################################################################
 
-curl "https://api.casalinovalerio.com/short-url.php?url=$1"
+[ $# -eq 1 ] || { printf "One url at a time please...\\n"; exit 1; }
+
+curl "https://tinyurl.com/api-create.php?url=$1"
