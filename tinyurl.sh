ecf39bd095547720df6656f10b9b1faa18aa8a3e
casalinovalerio <casalinovalerio.cv@gmail.com>
2020-01-19 22:25:46 +0100

    Style and '''UX''' in tinyurl.sh

--- tinyurl.sh
+++ tinyurl.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env sh
 
 ###############################################################################
-# Script name		: url-shortener.sh
-# Author		: Even a monkey could do it
+# Script name		: tinyurl.sh
+# Author		: Valerio Casalino
 ###############################################################################
 
-[ $# -eq 1 ] || { printf "One url at a time please...\\n"; exit 1; }
+error() { printf "%s\\n" "$1"; exit 1; }
 
-curl "https://tinyurl.com/api-create.php?url=$1"
+usage() 
+{
+  cat <<-EOF
+  Usage: $( basename $0 ) http://example.com
+  
+  It will return a short link using tinyurl.com service
+  EOF  
+}
+	
+[ $# -eq 1 ] || { usage; exit 0;  }
+
+curl "https://tinyurl.com/api-create.php?url=$1" || error "Something is wrong" 
