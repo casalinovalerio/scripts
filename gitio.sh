ea61e833cbd83c8863e031d0216512729ed4fae2
casalinovalerio <casalinovalerio.cv@gmail.com>
2020-01-22 01:16:03 +0100

    Added custom code funcionality to gitio

--- gitio.sh
+++ gitio.sh
@@ -10,19 +10,40 @@ error() { printf "%s\\n" "$1"; exit 1; }
 
 usage() {
 	cat <<-EOF
-	Usage: $( basename $0 ) https://github.com/<anything>
+	Usage: $( basename $0 ) https://github.com/<anything> (opt: -c custom)
 	
-	Outputs https://git.io/<some-short-string>
+	Outputs https://git.io/(<some-short-string> or <custom>)
 	EOF
 }
 
 [ $# -ne 1 ] && error "You need 1 input (Github) url"
 
-out=$( curl -is https://git.io \
-	-F "url=$1" \
-| grep "Location:" \
-| cut -d' ' -f2 \
-|| error "No internet connection" ) 
+for arg in $@; do
+	case $arg in
+		-c)
+			[ -n "$code" ] && error "code already defined"
+			shift
+			code="$1"
+			shift
+			;;
+		*)
+			[ -n "$url" ] && error "Url already defined"
+			url="$1"
+			shift
+			;;
+	esac
+done
 
-[ -z "$out" ] && error "Invalid Github url"
+[ -z "$code" ] && \
+	out=$( curl -is https://git.io -F "url=$1" \
+		| grep "Location:" \
+		| cut -d' ' -f2 \
+		|| error "No internet connection" )
+[ -n "$code" ] && \
+	out=$( curl -is https://git.io -F "url=$1" -F "code=$code" \
+		| grep "Location:" \
+		| cut -d' ' -f2 \
+		|| error "No internet connection" )
+
+[ -z "$out" ] && error "Invalid Github url or invalid code"
 printf "%s\\n" "$out" 
